93659afd6de6a1fe68c249f00ee9872942f49045
aknair95 <aakashunair01031995@gmail.com>
2023-11-23 03:26:53 +0530

    storing new user data to reducer store

--- src/components/registrationForm/registrationForm.js
+++ src/components/registrationForm/registrationForm.js
@@ -1,8 +1,14 @@
-import { Container,Form,Button, Row, Col } from "react-bootstrap";
+import { Container,Form,Button,Row,Col } from "react-bootstrap";
 import classes from "./registrationForm.module.css"
 import { useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { userDatabaseActions } from "../../store/userDatabaseReducer";
+import axios from "axios";
+import { Alert } from "@mui/material";
 
 const RegistrationForm=() =>{
+    const dispatch=useDispatch();
+    const userData=useSelector((state) => state.userDatabase.userData );
     const firstNameRef=useRef();
     const lastNameRef=useRef();
     const ageRef=useRef();
@@ -10,37 +16,74 @@ const RegistrationForm=() =>{
     const mobNoRef=useRef();
     const emailRef=useRef();
 
-    const formSubmitHandler=(e) =>{
+    const formSubmitHandler=async(e) =>{
         e.preventDefault();
+        const enteredFirstName=firstNameRef.current.value;
+        const enteredLastName=lastNameRef.current.value;
+        const enteredAge=ageRef.current.value;
+        const uploadedFile=fileRef.current.value;
+        const enteredMobNo=mobNoRef.current.value;
+        const enteredEmail=emailRef.current.value;
+
+        const newUserData={
+            firstName: enteredFirstName,
+            lastName: enteredLastName,
+            age: enteredAge,
+            file: uploadedFile,
+            mobNo: enteredMobNo,
+            email: enteredEmail
+        }
+
+        dispatch(userDatabaseActions.addUserData(newUserData));
+
+        try{
+            await axios.patch(`https://admin-panel-bbe99-default-rtdb.firebaseio.com/userData.json`,{
+                newUserData
+            });
+            <Alert severity="success">Sent</Alert>  
+        } catch(error){
+            <Alert severity="danger">!!! Error !!!</Alert>
+        }
+
+        firstNameRef.current.value="";
+        lastNameRef.current.value="";
+        ageRef.current.value="";
+        fileRef.current.value="";
+        mobNoRef.current.value="";
+        emailRef.current.value="";
+
+        console.log(userData);
+
     }
+    
     return(
         <>
              <Container className={classes.formContainer} style={{width: "70vw",height: "max-content"}}>
                 <h3 className="p-2">REGISTRATION FORM</h3>
                 <Form onSubmit={formSubmitHandler}>
                     <Row className="p-3">
-                            <Form.Group as={Col}>
-                                <Form.Control
-                                type="text" 
-                                placeholder="Enter First Name" 
-                                required 
-                                size="md"
-                                width="auto" 
-                                ref={firstNameRef}/>
-                            </Form.Group>                 
-                            <Form.Group as={Col}>                 
-                                <Form.Control
-                                type="text" 
-                                placeholder="Enter Last Name" 
-                                required 
-                                size="md"
-                                width="auto" 
-                                ref={lastNameRef}/>
-                            </Form.Group>
+                        <Form.Group as={Col}>
+                            <Form.Control
+                            type="text" 
+                            placeholder="Enter First Name" 
+                            required 
+                            size="md"
+                            width="auto" 
+                            ref={firstNameRef}/>
+                        </Form.Group>                 
+                        <Form.Group as={Col}>                 
+                            <Form.Control
+                            type="text" 
+                            placeholder="Enter Last Name" 
+                            required 
+                            size="md"
+                            width="auto" 
+                            ref={lastNameRef}/>
+                        </Form.Group>
                     </Row>
                     <Row className="p-3">
                         <Form.Group as={Col}>      
-                            <Form.Control 
+                            <Form.Control
                             type="email" 
                             placeholder="Enter Email" 
                             required 
@@ -51,8 +94,9 @@ const RegistrationForm=() =>{
                         <Form.Group as={Col}>                
                             <Form.Control 
                             type="number" 
-                            placeholder="Enter Mobile No." 
-                            required 
+                            placeholder="Enter Mobile No."
+                            max={9999999999}  
+                            required
                             size="md"
                             width="auto" 
                             ref={mobNoRef}/>
@@ -62,7 +106,9 @@ const RegistrationForm=() =>{
                         <Form.Group as={Col}>                 
                             <Form.Control 
                             type="number" 
-                            placeholder="Enter Age" 
+                            placeholder="Enter Age"
+                            min={1}
+                            max={100} 
                             required 
                             size="md"
                             width="auto" 
