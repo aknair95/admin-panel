3c023925d9f77c913a9cb589b66896b7508dfa78
aknair95 <aakashunair01031995@gmail.com>
2023-11-22 18:07:56 +0530

    navigation bar added

---
+++ src/components/navbar/navigationBar.js
@@ -0,0 +1,50 @@
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import {  Nav,Button, Container, Navbar } from "react-bootstrap";
+import { authActions } from "../../store/authReducer";
+
+const NavigationBar=() =>{
+
+    const navigate=useNavigate();
+    const dispatch=useDispatch();
+
+    const activePath=document.location.pathname;
+    let homePage=false;
+    let loginPage=false;
+
+    if(activePath==="/"){
+        homePage=true;
+    }
+    else{
+        loginPage=true;
+    }                    
+
+    const token=localStorage.getItem("token");
+    dispatch(authActions.setToken(token));
+
+    const logoutHandler=() =>{
+        localStorage.removeItem("token");
+        localStorage.removeItem("emailId");
+
+        dispatch(authActions.logout());
+        dispatch(authActions.setEmailID(null));
+        dispatch(authActions.setToken(null));
+        navigate("/login");
+    }
+
+    return (
+        <>
+            <Navbar bg="dark" data-bs-theme="dark" fixed="top">
+                <Container>
+                    <Nav className="me-auto">
+                        <Nav.Link href="/" active={homePage}>HOME</Nav.Link>
+                        {token===null && <Nav.Link href="/login" active={loginPage}>LOGIN</Nav.Link>}
+                    </Nav>
+                    {!!token && <Button variant="danger" size="md" onClick={logoutHandler}>Logout</Button>} 
+                </Container>  
+            </Navbar>
+        </>
+    )
+}
+
+export default NavigationBar;
