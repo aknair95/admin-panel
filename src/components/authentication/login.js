54094d07d358758055dd799552608a0b7f4c5b7b
aknair95 <aakashunair01031995@gmail.com>
2023-11-22 20:58:29 +0530

    MUI snackbar configured for login page alerts

--- src/components/authentication/login.js
+++ src/components/authentication/login.js
@@ -1,16 +1,17 @@
 import classes from "./login.module.css";
 import { Button,Container,Form } from "react-bootstrap";
-import { useRef } from "react";
+import { useRef,useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useDispatch } from "react-redux";
 import { authActions } from "../../store/authReducer";
+import { Snackbar,Alert } from "@mui/material";
 
 const Login=() =>{
-    
     const emailRef=useRef();
     const passwordRef=useRef();
-    
+    const [successOpen,setSuccessOpen]=useState(false);
+    const [failOpen,setFailOpen]=useState(false);
     const dispatch=useDispatch();
     const navigate=useNavigate();
 
@@ -20,21 +21,22 @@ const Login=() =>{
         const enteredPassword=passwordRef.current.value; 
 
         try{ 
-            const response= await axios.post("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCU6Gv14nvJALzonBsNjtyx2RO_G4aW4BQ",{
+            const response= await axios.post("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBBnP-VDlrSXUhJleaLC3Dm-9UmlaZoZIs",{
                 email: enteredEmail,
                 password: enteredPassword,
                 returnSecureToken: true
              });
+             setSuccessOpen(true);
              localStorage.setItem("token",response.data.idToken);
              localStorage.setItem("emailId",enteredEmail);
-
              dispatch(authActions.login());
              dispatch(authActions.setEmailID(enteredEmail));
              dispatch(authActions.setToken(response.data.idToken));
-             navigate("/");
+             setTimeout(() =>{
+                navigate("/")
+             },5000);
             } catch(error){
-                console.log(error)
-                alert("!!! Incorrect Email or Password !!!");
+                setFailOpen(true);
             }
 
         emailRef.current.value="";
@@ -49,7 +51,13 @@ const Login=() =>{
         navigate("/signUp");
     }
 
+    const onCloseHandler=() =>{
+        setSuccessOpen(false);
+        setFailOpen(false);
+    }
+
     return(
+       <> 
         <Container className={classes.formContainer} style={{width: "80vw",height: "max-content"}}>
             <h3 className="p-2">LOGIN</h3>
             <Form onSubmit={loginHandler}>
@@ -75,7 +83,18 @@ const Login=() =>{
                     <Button variant="link" size="md" onClick={createNewAccHandler}>Create New Account? SignUp</Button>
                 </div>
             </Form>
-        </Container>     
+        </Container>
+        <Snackbar open={successOpen} autoHideDuration={5000} onClose={onCloseHandler}>
+            <Alert severity="success" sx={{ width: '100%' }}>
+                Successfully logged in !
+            </Alert>
+        </Snackbar>
+        <Snackbar open={failOpen} autoHideDuration={5000} onClose={onCloseHandler}>
+            <Alert severity="error" sx={{ width: '100%' }}>
+                Incorrect Email or Password.Try again !
+            </Alert>
+        </Snackbar>   
+       </>    
     )
 }
 
