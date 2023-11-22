259178b4453eb40ad19d34b99f283983ddd0b90b
aknair95 <aakashunair01031995@gmail.com>
2023-11-22 22:24:21 +0530

    navigating to login page if not logged in

--- src/components/homePage/home.js
+++ src/components/homePage/home.js
@@ -1,16 +1,29 @@
 
+import { Button } from "react-bootstrap";
 import classes from "./home.module.css";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const Home=() =>{
-    
-    // fetching token & email from local storage on app load
     const token=localStorage.getItem("token");
-    const emailId=localStorage.getItem("emailId");
-
+    const navigate=useNavigate();
+    useEffect(() =>{
+        if(token===null){
+            navigate("/login");
+        }
+    },);
+    
     return(
-        <header className={classes.hdr}>
-            <h2> Welcome to Admin Panel </h2><hr/>
-        </header>
+        <>
+            <header className={classes.hdr}>
+                <h2> Welcome to Admin Panel </h2><hr/>
+            </header>
+            <div className={classes.btns}>
+                <Button>ADMIN DASHBOARD</Button>
+                <Button>USER DASHBOARD</Button>
+            </div>
+        </>
+        
     )
 }
 
