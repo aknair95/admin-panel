a0756f8745d2923be6c08f7faa05d4630908dff0
aknair95 <aakashunair01031995@gmail.com>
2023-11-24 01:29:49 +0530

    user dashboard enlisting user details

--- src/components/userDashboard/userDashboard.js
+++ src/components/userDashboard/userDashboard.js
@@ -1,48 +1,57 @@
 import { useSelector } from "react-redux";
-import { Card, ListGroup, Container, Row, Col, Image, Button } from "react-bootstrap";
+import { ListGroup, Row, Col, Image, Button } from "react-bootstrap";
+import classes from "./userDashboard.module.css"
 
 const UserDashboard=() =>{
     const userData=useSelector((state) => state.userDatabase.userData );
-    console.log(userData)
     return(
-        <Card>
-         <ListGroup as="ul">
+        <div className={classes.userList}>
+            <ListGroup as="ul">
             { 
                 userData.map((item) =>{
                     return (
-                        <Card>
-                            <ListGroup.Item as="li">
-                                <Container>
-                                    <Row className="justify-content-md-center" key={Math.random()}>
-                                        <Col>
-                                            <h4>{item.firstName}</h4>
-                                        </Col>
-                                        <Col>
-                                            <h4>{item.lastName}</h4>
-                                        </Col>
-                                        <Col>
-                                            <h5>{item.age}</h5>
-                                        </Col>
-                                        <Col>
-                                            <h5>{item.email}</h5> 
-                                        </Col>
-                                        <Col>
-                                            <h5>{item.mobNo}</h5> 
-                                        </Col>
-                                        <Col>
-                                            <Image src={item.file} width="50px" height="50px"/>
-                                        </Col>
-                                        <Col>
+                            <div key={Math.random()} className={classes.userDetails}>
+                              <ListGroup.Item as="li">
+                                    <Row >
+                                        <Row>
+                                            <Col className={classes.col}>
+                                                <h5>First Name:</h5>
+                                                <p>{item.firstName}</p>
+                                            </Col>
+                                            <Col className={classes.col}>
+                                                <h5>Last Name:</h5>
+                                                <p>{item.lastName}</p>
+                                            </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col className={classes.col}>
+                                                <h5>Age:</h5>
+                                                <p>{item.age}</p>
+                                            </Col>
+                                            <Col className={classes.col}>
+                                                <h5>Email:</h5>
+                                                <p>{item.email}</p> 
+                                            </Col>
+                                        </Row>
+                                       <Row>
+                                            <Col className={classes.col}>
+                                                <h5>Mobile No:</h5>
+                                                <p>{item.mobNo}</p> 
+                                            </Col>
+                                            <Col className={classes.img}>
+                                                <Image src={item.file} width="100px" height="60px"/>
+                                            </Col>
+                                       </Row>
+                                        <Col className={classes.btn}>
                                             <Button>Update</Button>
                                         </Col>
                                     </Row>
-                                </Container>  
-                            </ListGroup.Item>
-                        </Card>)
-                        })
+                              </ListGroup.Item>
+                            </div>
+                )})
             }
-         </ListGroup>
-        </Card>
+            </ListGroup>
+        </div>  
     )
 }
 
