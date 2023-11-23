bc15f5a5bfd8bcd1f2008a3361340d74c2b3f6d9
aknair95 <aakashunair01031995@gmail.com>
2023-11-24 03:31:28 +0530

    user data custom component used

--- src/components/userDashboard/userDashboard.js
+++ src/components/userDashboard/userDashboard.js
@@ -1,9 +1,16 @@
 import { useSelector } from "react-redux";
-import { ListGroup, Row, Col, Image, Button } from "react-bootstrap";
+import { ListGroup, Row, Col, Button } from "react-bootstrap";
 import classes from "./userDashboard.module.css"
+import { useNavigate } from "react-router-dom";
+import UserDetailsCustom from "../userDetailsCustom";
 
 const UserDashboard=() =>{
-    const userData=useSelector((state) => state.userDatabase.userData );
+    const userData=useSelector((state) =>state.userDatabase.userData);
+    const navigate=useNavigate();
+    const updateBtnHandler=(e) =>{
+        navigate("/registrationForm");
+    }
+
     return(
         <div className={classes.userList}>
             <ListGroup as="ul">
@@ -12,38 +19,16 @@ const UserDashboard=() =>{
                     return (
                             <div key={Math.random()} className={classes.userDetails}>
                               <ListGroup.Item as="li">
-                                    <Row >
-                                        <Row>
-                                            <Col className={classes.col}>
-                                                <h5>First Name:</h5>
-                                                <p>{item.firstName}</p>
-                                            </Col>
-                                            <Col className={classes.col}>
-                                                <h5>Last Name:</h5>
-                                                <p>{item.lastName}</p>
-                                            </Col>
-                                        </Row>
-                                        <Row>
-                                            <Col className={classes.col}>
-                                                <h5>Age:</h5>
-                                                <p>{item.age}</p>
-                                            </Col>
-                                            <Col className={classes.col}>
-                                                <h5>Email:</h5>
-                                                <p>{item.email}</p> 
-                                            </Col>
-                                        </Row>
-                                       <Row>
-                                            <Col className={classes.col}>
-                                                <h5>Mobile No:</h5>
-                                                <p>{item.mobNo}</p> 
-                                            </Col>
-                                            <Col className={classes.img}>
-                                                <Image src={item.file} width="100px" height="60px"/>
-                                            </Col>
-                                       </Row>
+                                    <Row>
+                                        <UserDetailsCustom
+                                         firstName={item.firstName}
+                                         lastName={item.lastName}
+                                         age={item.age}
+                                         email={item.email}
+                                         mobNo={item.mobNo}
+                                         file={item.file}/>
                                         <Col className={classes.btn}>
-                                            <Button>Update</Button>
+                                            <Button onClick={updateBtnHandler}>Update</Button>
                                         </Col>
                                     </Row>
                               </ListGroup.Item>
